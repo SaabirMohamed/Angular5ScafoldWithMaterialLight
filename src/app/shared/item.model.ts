6e177a1f184cc52f06f65b6dd6cf5509885e58e9
Saabir <saabirgmohamed@gmail.com>
2018-03-03 20:41:40 +0200

    added seo meta to index and seo service

--- src/app/shared/item.model.ts
+++ src/app/shared/item.model.ts
@@ -1,7 +1,86 @@
-export interface Item {
+export interface PageSeo {
+    pageTitle: string;
+    author: string;
+    keywords: string;
+    dateUpdated: string;
+    addedBy: string;
+  }
+/*
+
+//   title.setTitle('Translations Arabic English');
+//   meta.addTags([
+//     { name: 'author',   content: 'Mahadeeni.co.co.za'},
+//     { name: 'keywords', content: 'translation services, arabic to english and english to arabic'},
+//     { name: 'description', content: 'Offering professional translation services to Arabic from English and Afrikaans' }
+//   ]);
+
+*/
+
+  export interface HomePage {
+    pageTitle: string;
+    pageDescription: string;
+    titleImage: string;
+    headerBgColor: string;
+    contentBgColor: string;
+    dateUpdated: string;
+    addedBy: string;
+  }
+
+  export interface CounsellingPage {
+    pageTitle: string;
+    pageDescription: string;
+    titleImage: string;
+    headerBgColor: string;
+    contentBgColor: string;
+    dateUpdated: string;
+    addedBy: string;
+  }
+
+  export interface TranslationsPage {
+    pageTitle: string;
+    pageDescription: string;
+    titleImage: string;
+    headerBgColor: string;
+    contentBgColor: string;
+    dateUpdated: string;
+    addedBy: string;
+  }
+
+  export interface PageSections {
+    sectionTitle: string;
+    sectionDescription: string;
+    sectionImage: string;
+    dateUpdated: string;
+    addedBy: string;
+  }
+
+  export interface Item {
     pageTitle: string;
     pageDescription: string;
     headerBgColor: string;
     contentBgColor: string;
     titleImage: string;
   }
+
+  export interface ContactInfo {
+      companyName: string;
+      companyStreet: string;
+      companySuburb: string;
+      companyCountry: string;
+      companyLatitude: string;
+      companyLongitude: string;
+      companyPhone: number;
+      companyFax: number;
+      companyEmail: string;
+      companyTwitter: string;
+      companyFacebook: string;
+      companyGoogle: string;
+  }
+
+  export interface AboutInfo {
+    aboutTitle: string;
+    aboutDescription: string;
+}
+
+
+  
